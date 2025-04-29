43258087eb3252efe05d9d71a4d2a9f42e8423ba
GrommashPRD <epavel425@gmail.com>
2025-04-29 15:07:19 +0300

    исправления в коде

--- tracker_app/tasks.py
+++ tracker_app/tasks.py
@@ -7,6 +7,10 @@ from django.db import IntegrityError
 from tracker_app.serializers import DomainSerializer
 from tracker_app.TimesTamp import TimeConstants
 from .repository import DomainsInRange
+from prometheus_client import Counter
+
+DB_ERRORS_TOTAL = Counter('db_errors_total', 'Total number of database errors')
+DB_SPECIFIC_ERRORS_TOTAL = Counter('db_specific_errors_total', 'Total number of specific DatabaseErrors')
 
 logger = logging.getLogger(__name__)
 
@@ -35,12 +39,16 @@ def add_data_in_database(user_id:int , domains: List[str], repo=repo) -> None:
                 current_timestamp,
                 serializer_row_database.errors
             )
+            DB_SPECIFIC_ERRORS_TOTAL.inc()
+            DB_ERRORS_TOTAL.inc()
             continue
 
         try:
             repo.add_user_domain_history(user_id=user_id, domain=domain, timestamp=current_timestamp)
         except IntegrityError as exc:
             logger.error('Error during saving data; user_id: %s; domain: %s; created_at: %s; err: %s', )
+            DB_SPECIFIC_ERRORS_TOTAL.inc()
+            DB_ERRORS_TOTAL.inc()
             raise exc
     logger.info('Finished adding data to database')
 
